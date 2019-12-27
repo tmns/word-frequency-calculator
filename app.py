b3a1b9aa51e0b2c03dd2ab0c6474305cf5321f31
methias <tmn5@pm.me>
2019-12-27 20:21:51 +0200

    add new index route + remove test routes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -11,14 +11,9 @@ db = SQLAlchemy(app)
 from models import Result
 
 
-@app.route('/')
-def hello():
-    return 'Hello world!'
-
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
