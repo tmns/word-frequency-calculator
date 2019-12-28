61fef445cbe0027c6d64842346f1d4f58b5da80f
methias <tmn5@pm.me>
2019-12-28 17:46:55 +0200

    implement polling and render results

--- static/main.js
+++ static/main.js
@@ -3,8 +3,8 @@
 
 	angular.module('WordcountApp', [])
 
-	.controller('WordcountController', ['$scope', '$log', '$http', 
-		function($scope, $log, $http) {
+	.controller('WordcountController', ['$scope', '$log', '$http', '$timeout', 
+		function($scope, $log, $http, $timeout) {
 			$scope.getResults = function() {
 				$log.log('test');
 
@@ -13,11 +13,32 @@
 				$http.post('/start', {'url': userInput}).
 					success(function(results) {
 						$log.log(results);
+						getWordCount(results);
 					}).
 					error(function(error) {
 						$log.log(error);
 					})
 			};
+
+			function getWordCount(jobId) {
+				var timeout = '';
+
+				var poller = function() {
+					$http.get('/results/' + jobId).
+						success(function(data, status, headers, config) {
+							if (status === 202) {
+								$log.log(data, status);
+							} else if (status === 200) {
+								$log.log(data);
+								$scope.wordcounts = data;
+								$timeout.cancel(timeout);
+								return false;
+							}
+							timeout = $timeout(poller, 2000);
+						});
+				};
+				poller();
+			}
 		}
 	]);
 })();
