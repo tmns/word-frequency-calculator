6f8e4d3921b07ffb73124fcca59a649adf3c1c81
methias <tmn5@pm.me>
2019-12-28 22:27:50 +0200

    add error msg to ui

--- static/main.js
+++ static/main.js
@@ -7,6 +7,7 @@
 		function($scope, $log, $http, $timeout) {
 			$scope.submitButtonText = 'Submit';
 			$scope.loading = false;
+			$scope.urlerror = false;
 
 			$scope.getResults = function() {
 				var userInput = $scope.url;
@@ -14,6 +15,7 @@
 				$http.post('/start', {'url': userInput}).
 					success(function(results) {
 						$log.log(results);
+						$scope.urlerror = false;
 						$scope.loading = true;
 						$scope.submitButtonText = 'Loading...';
 						$scope.wordcounts = null;
@@ -41,6 +43,12 @@
 								return false;
 							}
 							timeout = $timeout(poller, 2000);
+						}).
+						error(function(error) {
+							$log.log(error);
+							$scope.loading = false;
+							$scope.submitButtonText = 'Submit';
+							$scope.urlerror = true;
 						});
 				};
 				poller();
